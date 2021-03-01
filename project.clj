762f0b81b3591d52b3fa4d8493007980073a4fda
Constantin Weißer <i7c@posteo.de>
2021-03-01 01:29:29 +0100

    Create IAM user for self-setup

--- project.clj
+++ project.clj
@@ -4,6 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/data.json "1.0.0"]
+                 [com.cognitect.aws/api "0.8.505"]
+                 [com.cognitect.aws/endpoints "1.1.11.960"]
+                 [com.cognitect.aws/iam "811.2.844.0"]
                  [clj-http "3.12.0"]]
   :main ^:skip-aot farina.core
   :target-path "target/%s"
--- src/farina/core.clj
+++ src/farina/core.clj
@@ -1,7 +1,34 @@
 (ns farina.core
+  (:require [clj-http.client :as client]
+            [cognitect.aws.client.api :as aws]
+            [cognitect.aws.credentials :as credentials])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def iam (aws/client {:api :iam
+                      :region "eu-central-1"}))
+
+(defn create-aws-user [username path]
+  (let [response (aws/invoke iam {:op :CreateUser
+                                  :request {:UserName username
+                                            :Path path}})
+        {{{:keys [Code Message]} :Error} :ErrorResponse
+         {:keys [Path UserName UserId] :as user} :User} response]
+    (if (some? Code)
+      (throw (IllegalStateException. Message))
+      user)))
+
+(defn get-or-create-aws-user [username path]
+  (let [response (aws/invoke iam {:op :GetUser
+                                  :request {:UserName username
+                                            :Path path}})
+        {{{:keys [Code Message]} :Error} :ErrorResponse
+         {:keys [Path UserName UserId] :as user} :User} response]
+    (if (some? Code)
+      (if (= Code "NoSuchEntity")
+        (create-aws-user username path)
+        (throw (IllegalStateException. Message)))
+      user)))
+
+
+(defn -main [& args]
+  (println (get-or-create-aws-user "farina" "/farina/")))
