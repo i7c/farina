b0baf9fdae03edbf2d5f288b15d04f04291831a9
Constantin Weißer <i7c@posteo.de>
2021-03-07 19:18:38 +0100

    Correct indentation

--- src/farina/infrastate.clj
+++ src/farina/infrastate.clj
@@ -8,6 +8,6 @@
       (let [new-state (everything state)
             [went came _] (diff state new-state)]
         (cond
-              (and (nil? went) (nil? came)) (assoc new-state :outcome :complete)
-              (<= iter-remaining 0) (assoc new-state :outcome :partial)
-              :else (recur new-state (dec iter-remaining)))))))
+          (and (nil? went) (nil? came)) (assoc new-state :outcome :complete)
+          (<= iter-remaining 0) (assoc new-state :outcome :partial)
+          :else (recur new-state (dec iter-remaining)))))))
