952e8d7854ddddabd2f5b6135f2740bdb755d2d4
Constantin Weißer <i7c@posteo.de>
2021-03-07 16:05:50 +0100

    Add iteration limit for stabilizing state

--- src/farina/infrastate.clj
+++ src/farina/infrastate.clj
@@ -3,9 +3,11 @@
 
 (defn spawn [initial-state brood]
   (let [everything (apply comp (reverse brood))]
-    (loop [state initial-state]
+    (loop [state initial-state
+           iter-remaining 1000]
       (let [new-state (everything state)
             [went came _] (diff state new-state)]
-        (if (or (some? went) (some? came))
-          (recur new-state)
-          new-state)))))
+        (cond
+              (and (nil? went) (nil? came)) (assoc new-state :outcome :complete)
+              (<= iter-remaining 0) (assoc new-state :outcome :partial)
+              :else (recur new-state (dec iter-remaining)))))))
--- test/farina/infrastate_test.clj
+++ test/farina/infrastate_test.clj
@@ -4,17 +4,16 @@
             [farina.infrastate :refer :all]))
 
 (deftest empty-state-and-empty-infra-returns-empty-state
-  (let [resulting (spawn {} [])
-        [a b _] (diff resulting {})]
+  (let [result (spawn {} [])
+        [a b _] (diff result {:outcome :complete})]
 
     (is (and (nil? a) (nil? b)))))
 
 (deftest state-accumulates
   (let [brood [(fn [state] (assoc state :a 10))
                (fn [state] (assoc state :b (+ (:a state) 5)))]
-
         result (spawn {} brood)
-        [a b _] (diff result {:a 10 :b 15})]
+        [a b _] (diff result {:a 10 :b 15 :outcome :complete})]
 
     (is (and (nil? a) (nil? b)))))
 
@@ -23,10 +22,16 @@
                  (if-let [dep (:b state)]
                    (assoc state :a (* dep 2))
                    state))
-
                (fn [state] (assoc state :b 10))]
-
         result (spawn {} brood)
-        [a b _] (diff result {:a 20 :b 10})]
+        [a b _] (diff result {:a 20 :b 10 :outcome :complete})]
 
+    (is (and (nil? a) (nil? b)))))
+
+(deftest cancel-if-state-is-never-stable
+  (let [brood [(fn [state] (assoc state :a (+ (or (:a state) 0) 1)))]
+        result (spawn {} brood)
+        [a b _] (diff result {:a 1001 :outcome :partial})]
+
+    (println a b result)
     (is (and (nil? a) (nil? b)))))
