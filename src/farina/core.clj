6120dfb31ecdcda461fa62e61a95f860fb81f67a
Constantin Weißer <i7c@posteo.de>
2021-03-21 14:22:04 +0100

    Add crunch function to transform and store DWD data

--- src/farina/core.clj
+++ src/farina/core.clj
@@ -1,13 +1,15 @@
 (ns farina.core
   (:require [farina.infra :as infra]
-            [farina.awsclient :refer [s3]]
+            [farina.awsclient :refer [s3 dynamo]]
+            [farina.dwd]
             [clojure.string :as s]
             [clojure.data.json :as json]
             [cognitect.aws.client.api :as aws]
             [clj-http.client :as client]
             [clojure.pprint :refer [pprint]])
   (:gen-class
-    :methods [^:static [download [] String]]))
+    :methods [^:static [download [] String]
+              ^:static [crunch [String] String]]))
 
 (def basename "farina")
 
@@ -25,6 +27,34 @@
         message (get-in response [:Error :Message])]
     (if (some? code) (throw (IllegalStateException. message)))
     (json/write-str response)))
+
+(defn get-object [bucket file]
+  (let [response (aws/invoke @s3 {:op :GetObject :request {:Bucket bucket
+                                                           :Key file}})]
+    (if (some? (:cognitect.anomalies/category response))
+      (throw (IllegalStateException. (str response)))
+      (slurp (:Body response)))))
+
+(defn -crunch [file]
+  (let [raw (get-object basename file)
+        json (json/read-str raw)
+        extracted (farina.dwd/extract-dwd-pollen-data json)
+        dynamodb-format (map #(do {:PutRequest
+                                   {:Item
+                                    {"date" {:N (str (:date %))}
+                                     "space" {:S (str (:region %) "-" (:kind %))}
+                                     "intensity" {:N (str (:intensity %))}}}})
+                             extracted)]
+    (let [result
+          (->> dynamodb-format
+               (partition-all 25)
+               (map #(aws/invoke @dynamo
+                                 {:op :BatchWriteItem
+                                  :request {:RequestItems
+                                            {"farina-germany" %}}})))
+          unprocessed (map #(:UnprocessedItems %) result)]
+      (if (not-every? empty? unprocessed)
+        (throw (IllegalStateException. "Not all items could be processed"))))))
 
 (defn -main [& args]
   (cond
