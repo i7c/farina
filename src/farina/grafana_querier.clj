ea88fab7c951ed29c75fb74db8e690355cfda8f0
Constantin Weißer <i7c@posteo.de>
2021-04-22 22:46:41 +0200

    Query the database for intensities

--- src/farina/grafana_querier.clj
+++ src/farina/grafana_querier.clj
@@ -1,6 +1,8 @@
 (ns farina.grafana-querier
   (:require [clojure.java.io :as io]
-            [clojure.data.json :as json])
+            [clojure.data.json :as json]
+            [farina.awsclient :as client]
+            [cognitect.aws.client.api :as aws])
   (:import [com.amazonaws.services.lambda.runtime RequestStreamHandler])
   (:gen-class
     :name farina.GrafanaHandler
@@ -8,6 +10,17 @@
 
 (defn -handleRequest [_ in out context]
   (with-open [writer (io/writer out)
-              reader (io/reader in)
-              request (json/read-str (slurp reader))]
-    (.write writer (str request))))
+              reader (io/reader in)]
+    (let [request (json/read-str (slurp reader))
+          space (get-in request ["queryStringParameters" "space"] "120122-alder")
+          rawdata (aws/invoke @client/dynamo
+                              {:op :Query
+                               :request {:TableName "farina-germany"
+                                         :KeyConditionExpression "#spce = :s"
+                                         :ExpressionAttributeValues {":s" {:S space}}
+                                         :ExpressionAttributeNames {"#spce" "space"}
+                                         :ScanIndexForward false}})
+          data (map #(do {:intensity (get-in % [:intensity :N])
+                          :date (get-in % [:date :N])})
+                    (:Items rawdata))]
+      (.write writer (json/write-str data)))))
